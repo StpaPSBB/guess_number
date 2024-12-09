5ebc2e23cbc94b400b2ba447bb5c5660cccc89ad
Stepa <stepanleonidov9510@gmail.com>
2024-12-09 11:35:31 +0300

    Created project

--- guess_number.py
+++ guess_number.py
@@ -2,11 +2,14 @@ from random import randint
 
 
 number = randint(1, 100)
+print(f"Game 'Guess number in range from 1 to 100'")
 
 while True:
+    #Asking user to guess a number
     guess = int(input("Guess a number: "))
+
     if guess == number:
-        print('You Guessed!')
+        print('You Guessed!') #print the winning message
         break
     elif guess > number:
         print('Too much!')
